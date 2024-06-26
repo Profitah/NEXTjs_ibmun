4f527b0f759a2ed9c646e219e0d56a45c565b462
I, yoonah <qwer120542@gmail.com>
2024-06-26 21:09:56 +0900

    Styles : Emotion 스타일링

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
-import Head from "next/head";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 import Image from "next/image";
 import Link from "next/link";
-import styles from "@/styles/Home.module.css";
-import { useState } from "react";
-import { useRouter } from "next/router";
+
+const imgBox = css`
+  width: 400px;
+  height: 200px;
+  overflow: hidden;
+  position: relative;
+`;
+
+const img = css`
+  position: absolute;
+  top: -50px;
+  left: 0;
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+`;
 
 export default function Home() {
   const [name, setName] = useState("");
@@ -11,14 +27,17 @@ export default function Home() {
 
   return (
     <>
+      <div css={imgBox}>
+        <Image src="/img/idol.png" alt="우주최강아이돌" width={400} height={200} css={img} />
+      </div>
       <div>
         <p>한국의 아이돌</p>
-        <Link href="/celebrity/ive" legacyBehavior>
+        <Link href="/celebrity/ive" legacyBehavior> {/* 현재 Next 버전에서 a태그에 Link를 사용하기 위해서 legacyBehavior가 반드시 필요.*/}
           <a>Move to '/celebrity/ive'</a>
         </Link>
 
         <p>한국의 아이돌2</p>
-        <Link href="/celebrity/idol/[name]" as="/celebrity/idol/newjeans" legacyBehavior>
+        <Link href="/celebrity/idol/[name]" as={`/celebrity/idol/newjeans`} legacyBehavior>
           <a>Move to '/celebrity/idol/newjeans'</a>
         </Link>
 
@@ -39,3 +58,4 @@ export default function Home() {
     </>
   );
 }
+
